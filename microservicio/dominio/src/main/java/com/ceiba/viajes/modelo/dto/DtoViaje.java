45c67a726e9c833fe04cc863a9cd8fc4fa76a357
Jeison Jesús Galeano Bolaño - Ceiba Software <jeison.galeano@ceiba.com.co>
2022-04-27 11:04:58 -0500

    Subida de cambios en clases deprecadas

--- microservicio/dominio/src/main/java/com/ceiba/viajes/modelo/dto/DtoViaje.java
+++ microservicio/dominio/src/main/java/com/ceiba/viajes/modelo/dto/DtoViaje.java
@@ -1,14 +1,19 @@
 package com.ceiba.viajes.modelo.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Getter
+@AllArgsConstructor
 public class DtoViaje {
 
     private Long id;
+    @NotNull(message = "El ID Usuario es obligatorio")
     private Long idUsuario;
+    @NotNull(message = "El ID Conductor es obligatorio")
     private Long idConductor;
     private Long tonelads;
     private Long tipoVehiculo;
@@ -20,20 +25,5 @@ public class DtoViaje {
     private String tipoCasa;
     private Long precio;
 
-    public DtoViaje(Long id, Long idUsuario, Long idConductor, Long tonelads,
-                    Long tipoVehiculo, LocalDateTime fechaCreacion, LocalDateTime
-                            fechaServicio, String origen, String destino, Boolean terminado, String tipoCasa, Long precio) {
-        this.id = id;
-        this.idUsuario = idUsuario;
-        this.idConductor = idConductor;
-        this.tonelads = tonelads;
-        this.tipoVehiculo = tipoVehiculo;
-        this.fechaCreacion = fechaCreacion;
-        this.fechaServicio = fechaServicio;
-        this.origen = origen;
-        this.destino = destino;
-        this.terminado = terminado;
-        this.tipoCasa = tipoCasa;
-        this.precio = precio;
-    }
+
 }
